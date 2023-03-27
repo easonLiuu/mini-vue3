c9a87f148f83f57d17d849e98b23cf2c55bae90d
liujiarui <ljr3289595084@163.com>
2023-03-27 21:21:08 +0800

    feat: 实现effect的stop方法

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,12 +1,33 @@
+import { extend } from "../shared";
+
 class ReactiveEffect{
-    private _fn: any
-    constructor(fn, public scheduler?){
+    private _fn: any;
+    deps = [];
+    active = true;
+    onStop?: () => void;
+    public scheduler: Function | undefined;
+    constructor(fn, scheduler?: Function){
         this._fn = fn
+        this.scheduler = scheduler
     }
     run() {
         activeEffect = this
         return this._fn()
     }
+    stop () {
+        if (this.active) {
+            cleanUpEffect(this)
+            if (this.onStop) {
+                this.onStop()
+            }
+            this.active = false
+        }
+    }
+}
+function cleanUpEffect (effect) {
+    effect.deps.forEach((dep: any) => {
+        dep.delete(effect)
+    });
 }
 const targetMap = new Map()
 export function track(target, key) {
@@ -22,7 +43,10 @@ export function track(target, key) {
         dep = new Set()
         depsMap.set(key, dep)
     }
+    if (!activeEffect) return
     dep.add(activeEffect)
+    //反向收集
+    activeEffect.deps.push(dep)
     // const dep = new Set()
 }
 export function trigger(target, key) {
@@ -41,6 +65,16 @@ let activeEffect
 export function effect (fn, options: any = {}) {
     //fn
     const _effect = new ReactiveEffect(fn, options.scheduler)
+    // Object.assign(_effect, options)
+    // extends
+    extend(_effect, options)
+    // _effect.onStop = options.onStop
     _effect.run()
-    return _effect.run.bind(_effect)
+    const runner: any = _effect.run.bind(_effect)
+    runner.effect = _effect
+    return runner
+}
+
+export function stop (runner) {
+    runner.effect.stop()
 }
