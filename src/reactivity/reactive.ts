7b90390e284bbf347fd262143f208d4208da3246
liujiarui <ljr3289595084@163.com>
2023-03-28 11:02:11 +0800

    feat: 实现readonly&代码优化

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,21 +1,14 @@
-import { track, trigger } from "./effect"
+import { mutableHandlers, readonlyHandlers } from "./baseHandler"
 
 export function reactive (raw) {
-    return new Proxy(raw, {
-        get(target, key){
-            //{foo:1}
-            //foo
-            const res = Reflect.get(target, key)
-            //依赖收集
-            track(target, key)
-            return res
-        },
-        set(target, key, value){
-            const res = Reflect.set(target, key, value)
-            //触发依赖
-            trigger(target, key)
-            return res
-        }
-    })
-
+    return createActiveObject(raw, mutableHandlers)
 }
+
+export function readonly (raw) {
+    return createActiveObject(raw, readonlyHandlers)
+}
+
+function createActiveObject (raw: any, baseHandlers) {
+    return new Proxy(raw, baseHandlers)
+}
+
